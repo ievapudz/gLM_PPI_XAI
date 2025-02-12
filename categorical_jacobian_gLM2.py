3c820e343cf0e9f8ce54e56ea492c511da97446c
Ieva Pudžiuvelytė <pudziuvelyteieva@gmail.com>
2025-02-12 14:38:04 +0100

    Reduced the code for sorting of the columns of the dataframe.

--- categorical_jacobian_gLM2.py
+++ categorical_jacobian_gLM2.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 
-TQDM_BAR_FORMAT = '{l_bar}{bar}| {n_fmt}/{total_fmt} [elapsed: {elapsed} remaining: {remaining}]'
-
 import os
 from typing import List
 import pandas as pd
-from tqdm.notebook import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
-import bokeh.plotting
-from bokeh.transform import linear_cmap
-from bokeh.plotting import figure, show
-from bokeh.palettes import viridis
+import numpy as np
+from matplotlib.colors import Normalize, LinearSegmentedColormap, to_hex
+
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 from matplotlib.colors import to_hex
-from bokeh.io import export_png
 
 #@markdown ## Flags
 fast = False # @param {type:"boolean"}
@@ -25,7 +20,8 @@ import os
 import torch
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 
-MODEL_PATH = "./gLM2_650M"
+MODEL_PATH = "./gLM2_650M" # on cluster
+
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 NUC_TOKENS = tuple(range(29, 33)) # 4 nucleotides a,t,c,g
 AA_TOKENS = tuple(range(4,24)) # 20 amino acids
@@ -34,6 +30,52 @@ NUM_TOKENS = len(AA_TOKENS) + len(NUC_TOKENS)
 model = AutoModelForMaskedLM.from_pretrained(MODEL_PATH, trust_remote_code=True).eval().to(DEVICE)
 tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, trust_remote_code=True)
 MASK_TOKEN_ID = tokenizer.mask_token_id
+
+def create_figure(df):
+    # df - [DataFrame] with columns 'i', 'j', and 'value'
+
+    # Sorting the columns of the pivoted dataframe
+    pivot_df = df.pivot(index='i', columns='j', values='value')
+    sorted_cols = sorted([int(item) for item in pivot_df.columns], key=int)
+    sorted_cols = [str(item) for item in sorted_cols]
+    pivot_df = pivot_df[sorted_cols]
+
+    # Sorting the rows
+    pivot_df.index = pivot_df.index.astype(int)
+    pivot_df = pivot_df.sort_index()
+
+    # Convert the pivot table to a 2D numpy array
+    array_2d = pivot_df.to_numpy()
+
+    # Normalize the data using the MinMax normalization and creating the colourmap
+    vmin = array_2d.min()
+    vmax = np.percentile(array_2d, 99)
+    norm = Normalize(vmin=vmin, vmax=vmax)
+
+    cmap = plt.get_cmap("Blues")
+    blues = [cmap(i) for i in np.linspace(0, 1, 256)]
+    custom_cmap = LinearSegmentedColormap.from_list("custom_blues", blues, N=256)
+
+    # Plot the heatmap
+    fig, ax = plt.subplots()
+    ax.invert_yaxis()
+    cax = ax.imshow(array_2d, cmap=custom_cmap, norm=norm, aspect='auto', interpolation='none')
+
+    # Set x-axis title and ticks at the top
+    ax.xaxis.set_label_position('top')
+    ax.xaxis.tick_top()
+    ax.set_xlabel('Position i')
+    ax.set_ylabel('Position j')
+    ax.set_title('COEVOLUTION')
+
+    plt.colorbar(cax, ax=ax, label='Contact Value')
+
+    # Adjust layout to make space for the title
+    plt.tight_layout(rect=[0, 0, 1, 0.95])  # Adjust the rect parameter to make space for the title
+
+    # Save the figure with increased space for the title
+    plt.savefig('test.png', bbox_inches='tight', pad_inches=0.5, dpi=300)
+
 
 def contact_to_dataframe(con):
     sequence_length = con.shape[0]
@@ -48,28 +90,28 @@ def jac_to_contact(jac, symm=True, center=True, diag="remove", apc=True):
     Lx,Ax,Ly,Ay = X.shape
 
     if center:
-      for i in range(4):
-        if X.shape[i] > 1:
-          X -= X.mean(i,keepdims=True)
+        for i in range(4):
+            if X.shape[i] > 1:
+                X -= X.mean(i,keepdims=True)
 
     contacts = np.sqrt(np.square(X).sum((1,3)))
 
     if symm and (Ax != 20 or Ay != 20):
-      contacts = (contacts + contacts.T)/2
+        contacts = (contacts + contacts.T)/2
 
     if diag == "remove":
-      np.fill_diagonal(contacts,0)
+        np.fill_diagonal(contacts,0)
 
     if diag == "normalize":
-      contacts_diag = np.diag(contacts)
-      contacts = contacts / np.sqrt(contacts_diag[:,None] * contacts_diag[None,:])
+        contacts_diag = np.diag(contacts)
+        contacts = contacts / np.sqrt(contacts_diag[:,None] * contacts_diag[None,:])
 
     if apc:
-      ap = contacts.sum(0,keepdims=True) * contacts.sum(1, keepdims=True) / contacts.sum()
-      contacts = contacts - ap
+        ap = contacts.sum(0,keepdims=True) * contacts.sum(1, keepdims=True) / contacts.sum()
+        contacts = contacts - ap
 
     if diag == "remove":
-      np.fill_diagonal(contacts,0)
+        np.fill_diagonal(contacts,0)
 
     return contacts
 
@@ -91,32 +133,32 @@ def get_categorical_jacobian(sequence: str, fast: bool = False):
 
     input_ids = input_ids.unsqueeze(0).to(DEVICE)
     with torch.no_grad(), torch.cuda.amp.autocast(enabled=True):
-      f = lambda x:model(x)[0][..., all_tokens].cpu().float()
+        f = lambda x:model(x)[0][..., all_tokens].cpu().float()
 
-      x = torch.clone(input_ids).to(DEVICE)
-      ln = x.shape[1]
+        x = torch.clone(input_ids).to(DEVICE)
+        ln = x.shape[1]
 
-      fx = f(x)[0]
-      if fast:
-        fx_h = torch.zeros((ln, 1 , ln, num_tokens), dtype=torch.float32)
-      else:
-        fx_h = torch.zeros((ln,num_tokens,ln,num_tokens),dtype=torch.float32)
-        x = torch.tile(x,[num_tokens,1])
-      #with tqdm(total=ln, bar_format=TQDM_BAR_FORMAT) as pbar:
+        fx = f(x)[0]
+        if fast:
+            fx_h = torch.zeros((ln, 1 , ln, num_tokens), dtype=torch.float32)
+        else:
+            fx_h = torch.zeros((ln,num_tokens,ln,num_tokens),dtype=torch.float32)
+            x = torch.tile(x,[num_tokens,1])
+
         for n in range(ln): # for each position
-          x_h = torch.clone(x)
-          if fast:
-            x_h[:, n] = MASK_TOKEN_ID
-          else:
-            x_h[:, n] = torch.tensor(all_tokens)
-          fx_h[n] = f(x_h)
-          #pbar.update(1)
-      jac = fx_h-fx
-      valid_nuc = is_nuc_pos & is_nuc_token
-      valid_aa = is_aa_pos & is_aa_token
-      # Zero out other modality
-      jac = torch.where(valid_nuc | valid_aa, jac, 0.0)
-      contact = jac_to_contact(jac.numpy())
+            x_h = torch.clone(x)
+            if fast:
+                x_h[:, n] = MASK_TOKEN_ID
+            else:
+                x_h[:, n] = torch.tensor(all_tokens)
+            fx_h[n] = f(x_h)
+
+        jac = fx_h-fx
+        valid_nuc = is_nuc_pos & is_nuc_token
+        valid_aa = is_aa_pos & is_aa_token
+        # Zero out other modality
+        jac = torch.where(valid_nuc | valid_aa, jac, 0.0)
+        contact = jac_to_contact(jac.numpy())
     return jac, contact, tokens
 
 MOD_A = "MFLKVRAEKRLGNFRLNVDFEMGRDYCVLLGPTGAGKSVFLELIAGIVKPDRGEVRLNGADITPLPPERRGIGFVPQDYALFPHLSVYRNIAYGLRNVERVERDRRVREMAEKLGIAHLLDRKPARLSGGERQRVALARALVIQPRLLLLDEPLSAVDLKTKGVLMEELRFVQREFDVPILHVTHDLIEAAMLADEVAVMLNGRIVEKGKLKELFSAKNGEVAEFLSARNLLLKVSKILD"
@@ -125,20 +167,4 @@ MOD_C = "MRLLFSALLALLSSIILLFVLLPVAATVTLQLFNFDEFLKAASDPAVWKVVLTTYYAALISTLIAVIFGTP
 sequence = f"<+>{MOD_A}<+>{MOD_C}"
 J, contact, tokens = get_categorical_jacobian(sequence, fast=fast)
 df = contact_to_dataframe(contact)
-
-# Convert the dataframe to a pivot table for heatmap
-pivot_df = df.pivot(index='i', columns='j', values='value')
-
-# Create a heatmap using matplotlib
-fig, ax = plt.subplots()
-ax.invert_yaxis()
-ax.xaxis.set_label_position('top')
-ax.xaxis.tick_top()
-cax = ax.imshow(pivot_df, cmap=plt.colormaps["Blues"], aspect='auto')
-ax.set_xlabel('position i')
-ax.set_ylabel('position j')
-ax.set_title('Categorical Jacobian Heatmap')
-
-plt.colorbar(cax, ax=ax, label='Contact Value')
-
-plt.savefig('ModAC_jacobian.png')
+create_figure(df)
