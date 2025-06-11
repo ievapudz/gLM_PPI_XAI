dd3bee4e120bd2a36c3e492c491278391c917f2b
Ieva Pudžiuvelytė <pudziuvelyteieva@gmail.com>
2025-06-11 15:42:06 +0200

    Running Softmax model's 01 experiment.

---
+++ configs/make_cv_config.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DIR=$1
+FOLDS=5
+BASE_PREFIX=$2
+
+BASE="${DIR}/${BASE_PREFIX}base.yaml"
+echo "From $BASE"
+for i in $(seq 0 $(($FOLDS-1)))
+do
+	BASE="${DIR}/${BASE_PREFIX}base.yaml"
+    echo "Generating ${DIR}/${BASE_PREFIX}$i.yaml ..."
+	sed "s/kfold_idx: 0/kfold_idx: "$i"/g" $BASE | sed "s|0/metrics|"$i"/metrics|g" | sed "s|0/checkpoints|"$i"/checkpoints|g" | sed -E "s|^([ \t]+save_dir: .*/)[^/]+/?$|\1"$i"/|" > "${DIR}/"${BASE_PREFIX}${i}".yaml"	
+
+done
