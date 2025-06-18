8545da21e75680c3c0997eaa57492e702d53f58a
Ieva Pudžiuvelytė <pudziuvelyteieva@gmail.com>
2025-06-18 08:53:34 +0200

    Reverted back to the KFold for now.

--- gLM_CNN/dataloader.py
+++ gLM_CNN/dataloader.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import torch
 import os
 import torch.multiprocessing
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
 
 torch.multiprocessing.set_sharing_strategy('file_system')
 
@@ -123,8 +123,8 @@ class URQDataModule(LightningDataModule):
                     self.num_samples,
                     self.concat_type
                 )
-                kf = StratifiedKFold(n_splits=self.kfolds, shuffle=True, random_state=self.seed)
-                all_splits = [k for k in kf.split(full_dataset, full_dataset.get('label'))]
+                kf = KFold(n_splits=self.kfolds, shuffle=True, random_state=self.seed)
+                all_splits = [k for k in kf.split(full_dataset)]
                 train_indexes, val_indexes = all_splits[self.kfold_idx]
                 train_indexes, val_indexes = train_indexes.tolist(), val_indexes.tolist()
                 self.train_dataset = Subset(full_dataset, train_indexes)
